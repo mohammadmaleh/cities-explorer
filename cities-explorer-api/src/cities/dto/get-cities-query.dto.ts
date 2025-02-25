121b356811c6a961ab1a3895aa61eb7400564853
mohammadmaleh <mohammad.maleh@gmail.com>
2025-02-25 07:46:28 +0100

    added swagger

--- cities-explorer-api/src/cities/dto/get-cities-query.dto.ts
+++ cities-explorer-api/src/cities/dto/get-cities-query.dto.ts
@@ -1,19 +1,37 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class GetCitiesQueryDto {
+  @ApiPropertyOptional({
+    description: 'Search term for city name or country',
+    example: 'madrid',
+  })
   @IsString()
   @IsOptional()
   searchTerm?: string;
 
+  @ApiPropertyOptional({
+    description: 'Filter by continent',
+    example: 'Europe',
+  })
   @IsString()
   @IsOptional()
   continent?: string;
 
+  @ApiPropertyOptional({
+    description: 'Sort by field and direction (format: field:asc|desc)',
+    example: 'name:asc',
+  })
   @IsString()
   @IsOptional()
   sortBy?: string;
 
+  @ApiPropertyOptional({
+    description: 'Page number',
+    example: 1,
+    default: 1,
+  })
   @Type(() => Number)
   @IsNumber()
   page: number = 1;
--- cities-explorer-api/src/main.ts
+++ cities-explorer-api/src/main.ts
@@ -1,14 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cities Explorer API')
+    .setVersion('1.0')
+    .build();
+
   app.enableCors({
     origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
